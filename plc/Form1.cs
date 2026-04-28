f11efb7d5002df9f9531dd8b77b5efcb8d6e20ee
백경흠 <146094259+qorrudgma@users.noreply.github.com>
2026-04-28 16:36:58 +0900

    04/28 ASCII/HEX 상태에서 읽기/쓰기

--- plc/Form1.cs
+++ plc/Form1.cs
@@ -131,40 +131,40 @@ namespace PLC
 
                 // Subheader
                 // 데이터 형식
-                String subHeader = "5000";
+                string subHeader = "5000";
 
                 // Access route
                 // network number
-                String networkNo = "00";
+                string networkNo = "00";
                 // PC number
-                String pcNo = "FF";
+                string pcNo = "FF";
                 // I/O number
-                String IONo = "FF03";
+                string IONo = "FF03";
                 // station number
-                String stationNo = "00";
+                string stationNo = "00";
                 // length
-                String dataLen = "0C00";
+                string dataLen = "0C00";
                 // timer
-                String timer = "1000";
+                string timer = "1000";
 
                 // Request data
                 // command
-                String cmd = "0104"; // "0104" => read, "0114" => write
+                string cmd = "0104"; // "0104" => read, "0114" => write
                 //subcommand
-                String subCmd = "0000";
+                string subCmd = "0000";
                 // head device number(어디부터 읽을지)
                 //String hdNo = "640000";
-                String hdNo;
+                string hdNo;
                 // device code
                 //String dCode = "A8"; // D = A8
-                String dCode;
+                string dCode;
 
                 ParseAddress(readStart.Text, out dCode, out hdNo);
 
                 System.Diagnostics.Debug.WriteLine("dCode: " + dCode + ", hdNo: " + hdNo);
 
-                // device point number(데이터 읽을 길이)
-                String devicePointNo = "0500";
+                // device point number(몇 워드 읽을거냐)
+                string devicePointNo = "0500";
 
                 //string requestHex = "500000FFFF03000C00100001040000640000A80500";
                 string requestHex = subHeader + networkNo + pcNo + IONo + stationNo + dataLen + timer + cmd + subCmd + hdNo + dCode + devicePointNo;
@@ -285,154 +285,6 @@ namespace PLC
             }
         }
 
-        // 아스키
-        //private void button2_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        if (client == null || !client.Connected || stream == null)
-        //        {
-        //            MessageBox.Show("먼저 PLC 연결을 해주세요.");
-        //            return;
-        //        }
-
-        //        // Subheader
-        //        // 데이터 형식
-        //        String subHeader = "5000";
-
-        //        // Access route
-        //        // network number
-        //        String networkNo = "00";
-        //        // PC number
-        //        String pcNo = "FF";
-        //        // I/O number
-        //        String IONo = "FF03";
-        //        // station number
-        //        String stationNo = "00";
-        //        // length
-        //        String dataLen = "0C00";
-        //        // timer
-        //        String timer = "1000";
-
-        //        // Request data
-        //        // command
-        //        String cmd = "0104"; // "0104" => read, "0114" => write
-        //        //subcommand
-        //        String subCmd = "0000";
-        //        // head device number(어디부터 읽을지)
-        //        //String hdNo = "640000";
-        //        String hdNo;
-        //        // device code
-        //        //String dCode = "A8"; // D = A8
-        //        String dCode;
-
-        //        ParseAddress(readStart.Text, out dCode, out hdNo);
-
-        //        System.Diagnostics.Debug.WriteLine("dCode: " + dCode + ", hdNo: " + hdNo);
-
-        //        // device point number(데이터 읽을 길이)
-        //        String devicePointNo = "0500";
-
-        //        //string requestHex = "500000FFFF03000C00100001040000640000A80500";
-        //        string requestHex = subHeader + networkNo + pcNo + IONo + stationNo + dataLen + timer + cmd + subCmd + hdNo + dCode + devicePointNo;
-        //        //string requestHex = subHeader + networkNo + pcNo + IONo + stationNo + dataLen + timer + cmd + subCmd + dCode + hdNo + devicePointNo;
-        //        System.Diagnostics.Debug.WriteLine(requestHex);
-
-
-        //        byte[] requestBytes = new byte[requestHex.Length / 2];
-        //        for (int i = 0; i < requestBytes.Length; i++)
-        //        {
-        //            requestBytes[i] = Convert.ToByte(requestHex.Substring(i * 2, 2), 16);
-        //        }
-
-        //        // PLC로 전송
-        //        stream.Write(requestBytes, 0, requestBytes.Length);
-        //        stream.Flush();
-
-        //        // 응답 수신
-        //        byte[] buffer = new byte[1024];
-        //        int length = stream.Read(buffer, 0, buffer.Length);
-
-        //        // end code
-        //        // 코드 | 의미
-        //        // C051 | 주소 범위 오류
-        //        // C052 | 디바이스 지정 오류
-        //        // C054 | 읽기 길이 오류
-        //        // C059 | 지원 안 되는 명령
-
-        //        if (length <= 0)
-        //        {
-        //            MessageBox.Show("응답이 없습니다.");
-        //            return;
-        //        }
-
-        //        byte[] responseBytes = new byte[length];
-        //        Array.Copy(buffer, responseBytes, length);
-
-        //        string responseHex = BitConverter.ToString(responseBytes);
-        //        string responseAscii = Encoding.ASCII.GetString(responseBytes);
-
-        //        System.Diagnostics.Debug.WriteLine("요청 HEX : " + requestHex);
-        //        System.Diagnostics.Debug.WriteLine("수신 길이 : " + length);
-        //        System.Diagnostics.Debug.WriteLine("응답 HEX : " + responseHex);
-        //        System.Diagnostics.Debug.WriteLine("응답 ASCII : " + responseAscii);
-
-        //        // 최소 길이 확인
-        //        if (length < 11)
-        //        {
-        //            MessageBox.Show(
-        //                "응답 길이가 너무 짧습니다.\r\n" +
-        //                "수신 길이: " + length + "\r\n" +
-        //                "응답 HEX: " + responseHex
-        //            );
-        //            return;
-        //        }
-
-        //        // End Code 확인
-        //        ushort endCode = BitConverter.ToUInt16(responseBytes, 9);
-        //        if (endCode != 0x0000)
-        //        {
-        //            MessageBox.Show(
-        //                "PLC 에러 응답\r\n" +
-        //                "EndCode: 0x" + endCode.ToString("X4") + "\r\n" +
-        //                "응답 HEX: " + responseHex
-        //            );
-        //            return;
-        //        }
-
-        //        // 실제 데이터는 11바이트 이후
-        //        int dataStartIndex = 11;
-        //        int dataLength = length - dataStartIndex;
-
-        //        if (dataLength <= 0)
-        //        {
-        //            MessageBox.Show("정상 응답이지만 데이터가 없습니다.");
-        //            return;
-        //        }
-
-        //        byte[] dataBytes = new byte[dataLength];
-        //        Array.Copy(responseBytes, dataStartIndex, dataBytes, 0, dataLength);
-
-        //        string dataHex = BitConverter.ToString(dataBytes);
-        //        string dataAscii = Encoding.ASCII.GetString(dataBytes).TrimEnd('\0');
-
-        //        textBox3.Text = dataHex;
-        //        textBox5.Text = dataAscii;
-
-        //        //MessageBox.Show(
-        //        //    "요청 HEX: " + requestHex + "\r\n\r\n" +
-        //        //    "수신 길이: " + length + "\r\n" +
-        //        //    "전체 응답 HEX: " + responseHex + "\r\n\r\n" +
-        //        //    "데이터 HEX: " + dataHex + "\r\n" +
-        //        //    "데이터 ASCII: " + dataAscii
-        //        //);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show("읽기 실패: " + ex.Message);
-        //    }
-        //}
-
         // 쓰기 버튼(16 진수로 써서 저장하기)
         private void button3_Click(object sender, EventArgs e)
         {
@@ -452,42 +304,56 @@ namespace PLC
                     return;
                 }
 
-                String subHeader = "5000";
-                String networkNo = "00";
-                String pcNo = "FF";
-                String IONo = "FF03";
-                String stationNo = "00";
-                String dataLen = "0E00";
-                String timer = "1000";
+                string subHeader = "5000";
+                string networkNo = "00";
+                string pcNo = "FF";
+                string IONo = "FF03";
+                string stationNo = "00";
+                string dataLen = "0E00";
+                string timer = "1000";
 
-                String cmd = "0114";
-                String subCmd = "0000";
+                string cmd = "0114";
+                string subCmd = "0000";
 
-                String hdNo;
-                String dCode;
+                string hdNo;
+                string dCode;
                 ParseAddress(writeStart.Text, out dCode, out hdNo);
 
-                int count = int.Parse(writeLen.Text.Trim());
-                String devicePointNo = ToDevicePointNo(count);
+                //int count = int.Parse(writeLen.Text.Trim());
+                // 몇 워드 읽을거냐
+                //string devicePointNo = ToDevicePointNo(count);
+
 
                 string writeData = "";
+                int count = 0;
 
                 if (mode == "ASCII")
                 {
                     string ascii = writeDataBox.Text.Trim();
                     writeData = AsciiToHex(ascii);
-                }
-                else if (mode == "HEX")
-                {
-                    string input = writeDataBox.Text.Trim().Replace(" ", "").ToUpper();
 
-                    if (input.Length != count * 4)
+                    count = ascii.Length / 2;
+
+                    if (ascii.Length % 2 != 0)
                     {
                         MessageBox.Show("Word 길이에 맞게 입력하세요");
                         return;
                     }
 
-                    writeData = ToLittleEndian(input);
+                }
+                else if (mode == "HEX")
+                {
+                    string hex = writeDataBox.Text.Trim().Replace(" ", "").ToUpper();
+
+                    count = hex.Length / 4;
+
+                    if (hex.Length != count * 4 || hex.Length % 4 != 0)
+                    {
+                        MessageBox.Show("Word 길이에 맞게 입력하세요");
+                        return;
+                    }
+
+                    writeData = ToLittleEndian(hex);
                 }
                 else
                 {
@@ -496,6 +362,7 @@ namespace PLC
                 }
 
                 dataLen = GetDataLen(writeData);
+                string devicePointNo = ToDevicePointNo(count);
 
                 string requestHex = subHeader + networkNo + pcNo + IONo + stationNo + dataLen + timer + cmd + subCmd + hdNo + dCode + devicePointNo + writeData;
 
@@ -537,121 +404,6 @@ namespace PLC
             }
         }
 
-
-
-        // 쓰기 버튼 (아스키로)
-        //private void button3_Click(object sender, EventArgs e)
-        //{
-        //    try
-        //    {
-        //        if (client == null || !client.Connected || stream == null)
-        //        {
-        //            MessageBox.Show("먼저 PLC 연결");
-        //            return;
-        //        }
-
-        //        String subHeader = "5000";
-        //        String networkNo = "00";
-        //        String pcNo = "FF";
-        //        String IONo = "FF03";
-        //        String stationNo = "00";
-        //        String dataLen = "0E00";
-        //        String timer = "1000";
-
-        //        // 요청
-        //        String cmd = "0114"; // write
-        //        String subCmd = "0000";
-
-        //        // 어디부터 쓰기 시작할지
-        //        //String hdNo = "640000";
-        //        String hdNo;
-        //        //String dCode = "A8";
-        //        String dCode;
-        //        ParseAddress(writeStart.Text, out dCode, out hdNo);
-        //        //System.Diagnostics.Debug.WriteLine("dCode: " + dCode + ", hdNo: " + hdNo);
-
-
-        //        // 쓸 데이터 길이 1 => 00-00, 2 => 00-00-00-00
-        //        //String devicePointNo = "0100";
-        //        int count = int.Parse(writeLen.Text.Trim());
-        //        String devicePointNo = ToDevicePointNo(count);
-
-        //        // 쓰기 데이터
-        //        //String writeData = "0155";
-        //        String writeDataAscii = writeDataBox.Text.Trim();
-        //        System.Diagnostics.Debug.WriteLine("writeDataAscii => " + writeDataAscii);
-
-        //        String writeDataHex = AsciiToHex(writeDataAscii);
-        //        System.Diagnostics.Debug.WriteLine("writeDataHex => " + writeDataHex);
-
-        //        String writeData = writeDataHex;
-
-        //        if (writeData.Length != count * 4)
-        //        {
-        //            MessageBox.Show("Word 길이에 맞게 입력하세요");
-        //            return;
-        //        }
-
-        //        dataLen = GetDataLen(writeData);
-
-        //        // 패킷 생성
-        //        string requestHex = subHeader + networkNo + pcNo + IONo + stationNo + dataLen + timer + cmd + subCmd + hdNo + dCode + devicePointNo + writeData;
-
-        //        System.Diagnostics.Debug.WriteLine("요청 HEX : " + requestHex);
-
-        //        // HEX → byte[]
-        //        byte[] requestBytes = new byte[requestHex.Length / 2];
-        //        for (int i = 0; i < requestBytes.Length; i++)
-        //        {
-        //            requestBytes[i] = Convert.ToByte(requestHex.Substring(i * 2, 2), 16);
-        //        }
-
-        //        // ===== PLC 전송 =====
-        //        stream.Write(requestBytes, 0, requestBytes.Length);
-        //        stream.Flush();
-
-        //        // ===== 응답 수신 =====
-        //        byte[] buffer = new byte[1024];
-        //        int length = stream.Read(buffer, 0, buffer.Length);
-
-        //        if (length <= 0)
-        //        {
-        //            MessageBox.Show("응답이 없습니다.");
-        //            return;
-        //        }
-
-        //        byte[] responseBytes = new byte[length];
-        //        Array.Copy(buffer, responseBytes, length);
-
-        //        string responseHex = BitConverter.ToString(responseBytes);
-
-        //        System.Diagnostics.Debug.WriteLine("응답 HEX : " + responseHex);
-
-        //        // ===== End Code 확인 =====
-        //        if (length < 11)
-        //        {
-        //            MessageBox.Show("응답이 너무 짧음");
-        //            return;
-        //        }
-
-        //        ushort endCode = BitConverter.ToUInt16(responseBytes, 9);
-
-        //        if (endCode == 0x0000)
-        //        {
-        //            MessageBox.Show("쓰기 성공");
-        //            return;
-        //        }
-        //        else
-        //        {
-        //            MessageBox.Show("쓰기 실패 EndCode: 0x" + endCode.ToString("X4"));
-        //        }
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        MessageBox.Show("쓰기 실패: " + ex.Message);
-        //    }
-        //}
-
         // 길이 선택
         private string ToDevicePointNo(int count)
         {
@@ -663,6 +415,10 @@ namespace PLC
             // 리틀엔디안 변환
             return hex.Substring(2, 2) + hex.Substring(0, 2);
         }
+
+
+
+        // 데이터 길이 계산
         private string GetDataLen(string writeData)
         {
             int byteLen = (writeData.Length / 2) + 12;
